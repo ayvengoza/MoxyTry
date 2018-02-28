9cf6f85e638ca457cda624f1abb11847ed296f8d
Andrii Zastupailo <ayvengoza@gamil.com>
2018-02-28 14:24:18 +0200

    simple async

--- app/src/main/java/com/zastupailo/moxytry/HelloWorldPresenter.kt
+++ app/src/main/java/com/zastupailo/moxytry/HelloWorldPresenter.kt
@@ -1,7 +1,9 @@
 package com.zastupailo.moxytry
 
+import android.os.AsyncTask
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by ang on 27.02.18.
@@ -10,6 +12,26 @@ import com.arellomobile.mvp.MvpPresenter
 class HelloWorldPresenter : MvpPresenter<HelloWorldView> {
 
     constructor(){
-        viewState.showMessage(R.string.hello_world)
+        val asyncTask = object : AsyncTask<Void, Void, Void>(){
+            override fun doInBackground(vararg params: Void?): Void? {
+                sleepSecond()
+                return null
+            }
+
+            override fun onPostExecute(result: Void?) {
+                super.onPostExecute(result)
+                viewState.showMessage(R.string.hello_world)
+            }
+
+            fun sleepSecond(){
+                try {
+                    TimeUnit.SECONDS.sleep(3)
+                } catch (ignore : InterruptedException){
+
+                }
+            }
+        }
+
+        asyncTask.execute()
     }
 }
--- app/src/main/java/com/zastupailo/moxytry/MainActivity.kt
+++ app/src/main/java/com/zastupailo/moxytry/MainActivity.kt
@@ -2,6 +2,8 @@ package com.zastupailo.moxytry
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
 import android.widget.TextView
 import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
@@ -11,17 +13,18 @@ class MainActivity : MvpAppCompatActivity(), HelloWorldView {
     @InjectPresenter
     lateinit var mHelloWorldPresenter : HelloWorldPresenter
 
-    var mHelloView : TextView? = null
-
     override fun showMessage(message: Int) {
-        if (mHelloView != null) {
-            mHelloView!!.setText(message)
-        }
+        val messageTextView = TextView(this)
+        messageTextView.setText(message)
+        messageTextView.setTextSize(40f)
+        messageTextView.setGravity(Gravity.CENTER_HORIZONTAL)
+        val layout: ViewGroup = findViewById(R.id.activity_main)
+        layout.addView(messageTextView)
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mHelloView = findViewById(R.id.hello_view)
     }
 }
